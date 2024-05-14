93f3358e65983e36a9a4b49c98c128a051f989e9
Yuliyan Ivanov <Julyan.Sv.Ivanov@gmail.com>
2024-05-14 19:33:43 +0300

    Update

--- JavaAdvanced24/StacksAndQueuesExercises/B02BasicStackOperations.java
+++ JavaAdvanced24/StacksAndQueuesExercises/B02BasicStackOperations.java
@@ -1,4 +1,37 @@
 package StacksAndQueuesExercises;
 
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
 public class B02BasicStackOperations {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] input = Arrays.stream(scanner.nextLine().split(" "))
+                .mapToInt(Integer::parseInt).toArray();
+
+        int n = input[0];
+        int s = input[1];
+        int x = input[2];
+
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+
+        for (int count = 1; count <= n; count++) {
+            int number = scanner.nextInt();
+            stack.push(number);
+        }
+        for (int count = 1; count <= s; count++) {
+            stack.pop();
+        }
+
+        if (stack.isEmpty()) {
+            System.out.println(0);
+        } else if (stack.contains(x)) {
+            System.out.println("true");
+        } else {
+            System.out.println(Collections.min(stack));
+        }
+    }
 }
