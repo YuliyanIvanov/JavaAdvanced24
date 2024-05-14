17c694f017fee080242607e8d694dc6161a256ef
Yuliyan Ivanov <Julyan.Sv.Ivanov@gmail.com>
2024-05-14 20:03:28 +0300

    Update

--- JavaAdvanced24/StacksAndQueuesExercises/B03MaximumElement.java
+++ JavaAdvanced24/StacksAndQueuesExercises/B03MaximumElement.java
@@ -8,20 +8,20 @@ public class B03MaximumElement {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int N = Integer.parseInt(scanner.nextLine());
+        int n = Integer.parseInt(scanner.nextLine());
 
         Stack<Integer> stack = new Stack<>();
         Stack<Integer> maxStack = new Stack<>();
 
-        for (int i = 0; i < N; i++) {
+        for (int i = 0; i < n; i++) {
             StringTokenizer st = new StringTokenizer(scanner.nextLine());
             int type = Integer.parseInt(st.nextToken());
 
             if (type == 1) {
-                int X = Integer.parseInt(st.nextToken());
-                stack.push(X);
-                if (maxStack.isEmpty() || X >= maxStack.peek()) {
-                    maxStack.push(X);
+                int x = Integer.parseInt(st.nextToken());
+                stack.push(x);
+                if (maxStack.isEmpty() || x >= maxStack.peek()) {
+                    maxStack.push(x);
                 }
             } else if (type == 2) {
                 int removedElement = stack.pop();
