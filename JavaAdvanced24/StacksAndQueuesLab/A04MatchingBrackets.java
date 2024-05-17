e92e84fabcbe50e173b4e97acccb67e82db494e2
Yuliyan Ivanov <Julyan.Sv.Ivanov@gmail.com>
2024-05-17 12:50:27 +0300

    Update

--- JavaAdvanced24/StacksAndQueuesLab/A04MatchingBrackets.java
+++ JavaAdvanced24/StacksAndQueuesLab/A04MatchingBrackets.java
@@ -1,4 +1,26 @@
 package StacksAndQueuesLab;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Scanner;
+
 public class A04MatchingBrackets {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String expression = scanner.nextLine();
+        Deque<Integer> stack = new ArrayDeque<>();
+
+        for (int i = 0; i < expression.length(); i++) {
+            char currentChar = expression.charAt(i);
+
+            if (currentChar == '(') {
+                stack.push(i);
+            } else if (currentChar == ')') {
+                int startIndex = stack.pop();
+                String subExpression = expression.substring(startIndex, i + 1);
+                System.out.println(subExpression);
+            }
+        }
+    }
 }
