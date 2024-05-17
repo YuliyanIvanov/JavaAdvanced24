cbf882fbc970ec0b85b73a7f942819f54f0d60d3
Yuliyan Ivanov <Julyan.Sv.Ivanov@gmail.com>
2024-05-17 12:35:26 +0300

    Update

--- JavaAdvanced24/StacksAndQueuesLab/A02SimpleCalculator.java
+++ JavaAdvanced24/StacksAndQueuesLab/A02SimpleCalculator.java
@@ -1,4 +1,31 @@
 package StacksAndQueuesLab;
 
+import java.util.Scanner;
+import java.util.Stack;
+
 public class A02SimpleCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String expression = scanner.nextLine();
+        String[] tokens = expression.split(" ");
+        Stack<Integer> stack = new Stack<>();
+        int currentSign = 1;
+
+        for (String token : tokens) {
+            if (token.equals("+")) {
+                currentSign = 1;
+            } else if (token.equals("-")) {
+                currentSign = -1;
+            } else {
+                int number = Integer.parseInt(token);
+                stack.push(currentSign * number);
+            }
+        }
+        int result = 0;
+        while (!stack.isEmpty()) {
+            result += stack.pop();
+        }
+        System.out.println(result);
+    }
 }
