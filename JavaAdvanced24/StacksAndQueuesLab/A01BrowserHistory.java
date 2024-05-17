c222140e1c1fb967fa6fa97c3b9f443e8f757284
Yuliyan Ivanov <Julyan.Sv.Ivanov@gmail.com>
2024-05-17 11:27:00 +0300

    Update

--- JavaAdvanced24/StacksAndQueuesLab/A01BrowserHistory.java
+++ JavaAdvanced24/StacksAndQueuesLab/A01BrowserHistory.java
@@ -1,4 +1,35 @@
 package StacksAndQueuesLab;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Scanner;
+
 public class A01BrowserHistory {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Deque<String> history = new ArrayDeque<>();
+        String currentURL = "";
+
+        while (true) {
+            String command = scanner.nextLine();
+
+            if (command.equals("Home")) {
+                break;
+            } else if (command.equals("back")) {
+                if (history.isEmpty()) {
+                    System.out.println("no previous URLs");
+                } else {
+                    currentURL = history.pop();
+                    System.out.println(currentURL);
+                }
+            } else {
+                if (!currentURL.isEmpty()) {
+                    history.push(currentURL);
+                }
+                currentURL = command;
+                System.out.println(currentURL);
+            }
+        }
+    }
 }
