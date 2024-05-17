e953481bb0653f8c72b3fd2dc5d5421643226f70
Yuliyan Ivanov <Julyan.Sv.Ivanov@gmail.com>
2024-05-17 12:42:51 +0300

    Update

--- JavaAdvanced24/StacksAndQueuesLab/A03DecimalToBinary.java
+++ JavaAdvanced24/StacksAndQueuesLab/A03DecimalToBinary.java
@@ -1,4 +1,32 @@
 package StacksAndQueuesLab;
 
+import java.util.Scanner;
+import java.util.Stack;
+
 public class A03DecimalToBinary {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int decimalNumber = scanner.nextInt();
+
+        if (decimalNumber == 0) {
+            System.out.println("0");
+            return;
+        }
+
+        Stack<Integer> stack = new Stack<>();
+
+        while (decimalNumber > 0) {
+            int remainder = decimalNumber % 2;
+            stack.push(remainder);
+            decimalNumber /= 2;
+        }
+
+        StringBuilder binaryRepresentation = new StringBuilder();
+        while (!stack.isEmpty()) {
+            binaryRepresentation.append(stack.pop());
+        }
+
+        System.out.println(binaryRepresentation);
+    }
 }
