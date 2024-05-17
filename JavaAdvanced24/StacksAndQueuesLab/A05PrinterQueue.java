09653b0ddf8c95b6b44b064817db80f166c24ecf
Yuliyan Ivanov <Julyan.Sv.Ivanov@gmail.com>
2024-05-17 13:06:03 +0300

    Update

--- JavaAdvanced24/StacksAndQueuesLab/A05PrinterQueue.java
+++ JavaAdvanced24/StacksAndQueuesLab/A05PrinterQueue.java
@@ -1,4 +1,34 @@
 package StacksAndQueuesLab;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Scanner;
+
 public class A05PrinterQueue {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Deque<String> queue = new ArrayDeque<>();
+
+        while (true) {
+            String command = scanner.nextLine();
+
+            if (command.equals("print")) {
+                break;
+            } else if (command.equals("cancel")) {
+                if (queue.isEmpty()) {
+                    System.out.println("Printer is on standby");
+                } else {
+                    String canceledFile = queue.pollFirst();
+                    System.out.println("Canceled " + canceledFile);
+                }
+            } else {
+                queue.offer(command);
+            }
+        }
+
+        while (!queue.isEmpty()) {
+            System.out.println(queue.pollFirst());
+        }
+    }
 }
