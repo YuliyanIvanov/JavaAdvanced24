84473adeb6bf8207dfd4f37f654275a3a84a6c80
Yuliyan Ivanov <Julyan.Sv.Ivanov@gmail.com>
2024-05-21 07:00:55 +0300

    Update

--- JavaAdvanced24/StacksAndQueuesLab/A07MathPotato.java
+++ JavaAdvanced24/StacksAndQueuesLab/A07MathPotato.java
@@ -1,4 +1,48 @@
 package StacksAndQueuesLab;
 
+import java.util.*;
+
 public class A07MathPotato {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] kids = scanner.nextLine().split(" ");
+        int n = Integer.parseInt(scanner.nextLine());
+
+        Queue<String> queue = new PriorityQueue<>();
+        Collections.addAll(queue, kids);
+
+        int cycle = 1;
+        while (queue.size() > 1) {
+            for (int i = 1; i < n; i++) {
+                queue.add(queue.poll());
+            }
+            if (isPrime(cycle)) {
+                System.out.println("Prime " + queue.peek());
+            } else {
+                System.out.println("Removed " + queue.poll());
+            }
+            cycle++;
+        }
+
+        System.out.println("Last is " + queue.poll());
+    }
+
+    private static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        if (num == 2) {
+            return true;
+        }
+        if (num % 2 == 0) {
+            return false;
+        }
+        for (int i = 3; i <= Math.sqrt(num); i += 2) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
