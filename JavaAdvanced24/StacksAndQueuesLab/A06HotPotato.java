2b3c1ec2c5676a04944d16d66c9a3f8d8b4182fa
Yuliyan Ivanov <Julyan.Sv.Ivanov@gmail.com>
2024-05-21 06:30:43 +0300

    Update

--- JavaAdvanced24/StacksAndQueuesLab/A06HotPotato.java
+++ JavaAdvanced24/StacksAndQueuesLab/A06HotPotato.java
@@ -1,11 +1,27 @@
 package StacksAndQueuesLab;
 
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class A06HotPotato {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        String[] kids = scanner.nextLine().split(" ");
+        int n = Integer.parseInt(scanner.nextLine());
 
+        Queue<String> queue = new LinkedList<>();
+        Collections.addAll(queue, kids);
+
+        while (queue.size() > 1) {
+            for (int i = 1; i < n; i++) {
+                queue.add(queue.poll());
+            }
+            System.out.println("Removed " + queue.poll());
+        }
+
+        System.out.println("Last is " + queue.poll());
     }
 }
