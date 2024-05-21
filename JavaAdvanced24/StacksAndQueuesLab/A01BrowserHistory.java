7fead73e0041a77c46d73cf260f448fea7ab70f1
Yuliyan Ivanov <Julyan.Sv.Ivanov@gmail.com>
2024-05-21 07:06:24 +0300

    Update

--- JavaAdvanced24/StacksAndQueuesLab/A01BrowserHistory.java
+++ JavaAdvanced24/StacksAndQueuesLab/A01BrowserHistory.java
@@ -9,42 +9,26 @@ public class A01BrowserHistory {
         Scanner scanner = new Scanner(System.in);
 
         Deque<String> history = new ArrayDeque<>();
-        Deque<String> forward = new ArrayDeque<>();
         String currentURL = "";
 
-        label:
         while (true) {
             String command = scanner.nextLine();
 
-            switch (command) {
-                case "Home":
-                    break label;
-                case "back":
-                    if (history.isEmpty()) {
-                        System.out.println("no previous URLs");
-                    } else {
-                        forward.addFirst(currentURL);
-                        currentURL = history.pop();
-                        System.out.println(currentURL);
-                    }
-                    break;
-                case "forward":
-                    if (forward.isEmpty()) {
-                        System.out.println("no next URLs");
-                    } else {
-                        currentURL = forward.pop();
-                        history.push(currentURL);
-                        System.out.println(currentURL);
-                    }
-                    break;
-                default:
-                    if (!currentURL.isEmpty()) {
-                        history.push(currentURL);
-                        forward.clear();
-                    }
-                    currentURL = command;
+            if (command.equals("Home")) {
+                break;
+            } else if (command.equals("back")) {
+                if (history.isEmpty()) {
+                    System.out.println("no previous URLs");
+                } else {
+                    currentURL = history.pop();
                     System.out.println(currentURL);
-                    break;
+                }
+            } else {
+                if (!currentURL.isEmpty()) {
+                    history.push(currentURL);
+                }
+                currentURL = command;
+                System.out.println(currentURL);
             }
         }
     }
--- JavaAdvanced24/StacksAndQueuesLab/A08BrowserHistoryUpgrade.java
+++ JavaAdvanced24/StacksAndQueuesLab/A08BrowserHistoryUpgrade.java
@@ -1,4 +1,51 @@
 package StacksAndQueuesLab;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Scanner;
+
 public class A08BrowserHistoryUpgrade {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Deque<String> history = new ArrayDeque<>();
+        Deque<String> forward = new ArrayDeque<>();
+        String currentURL = "";
+
+        label:
+        while (true) {
+            String command = scanner.nextLine();
+
+            switch (command) {
+                case "Home":
+                    break label;
+                case "back":
+                    if (history.isEmpty()) {
+                        System.out.println("no previous URLs");
+                    } else {
+                        forward.addFirst(currentURL);
+                        currentURL = history.pop();
+                        System.out.println(currentURL);
+                    }
+                    break;
+                case "forward":
+                    if (forward.isEmpty()) {
+                        System.out.println("no next URLs");
+                    } else {
+                        currentURL = forward.pop();
+                        history.push(currentURL);
+                        System.out.println(currentURL);
+                    }
+                    break;
+                default:
+                    if (!currentURL.isEmpty()) {
+                        history.push(currentURL);
+                        forward.clear();
+                    }
+                    currentURL = command;
+                    System.out.println(currentURL);
+                    break;
+            }
+        }
+    }
 }
