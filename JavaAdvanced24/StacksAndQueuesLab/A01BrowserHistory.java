3e84b72c8c0195378679cd16dc440f80a557348b
Yuliyan Ivanov <Julyan.Sv.Ivanov@gmail.com>
2024-05-21 07:05:21 +0300

    Update

--- JavaAdvanced24/StacksAndQueuesLab/A01BrowserHistory.java
+++ JavaAdvanced24/StacksAndQueuesLab/A01BrowserHistory.java
@@ -9,26 +9,42 @@ public class A01BrowserHistory {
         Scanner scanner = new Scanner(System.in);
 
         Deque<String> history = new ArrayDeque<>();
+        Deque<String> forward = new ArrayDeque<>();
         String currentURL = "";
 
+        label:
         while (true) {
             String command = scanner.nextLine();
 
-            if (command.equals("Home")) {
-                break;
-            } else if (command.equals("back")) {
-                if (history.isEmpty()) {
-                    System.out.println("no previous URLs");
-                } else {
-                    currentURL = history.pop();
+            switch (command) {
+                case "Home":
+                    break label;
+                case "back":
+                    if (history.isEmpty()) {
+                        System.out.println("no previous URLs");
+                    } else {
+                        forward.addFirst(currentURL);
+                        currentURL = history.pop();
+                        System.out.println(currentURL);
+                    }
+                    break;
+                case "forward":
+                    if (forward.isEmpty()) {
+                        System.out.println("no next URLs");
+                    } else {
+                        currentURL = forward.pop();
+                        history.push(currentURL);
+                        System.out.println(currentURL);
+                    }
+                    break;
+                default:
+                    if (!currentURL.isEmpty()) {
+                        history.push(currentURL);
+                        forward.clear();
+                    }
+                    currentURL = command;
                     System.out.println(currentURL);
-                }
-            } else {
-                if (!currentURL.isEmpty()) {
-                    history.push(currentURL);
-                }
-                currentURL = command;
-                System.out.println(currentURL);
+                    break;
             }
         }
     }
