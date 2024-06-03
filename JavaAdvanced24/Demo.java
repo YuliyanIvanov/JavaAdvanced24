465ef58abf214582ca7d3d339bceb8626083883e
Yuliyan Ivanov <Julyan.Sv.Ivanov@gmail.com>
2024-06-03 13:47:58 +0300

    Update

--- JavaAdvanced24/Demo.java
+++ JavaAdvanced24/Demo.java
@@ -1,5 +1,10 @@
+import java.util.*;
+
 public class Demo {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner scanner = new Scanner(System.in);
+
+
     }
 }
---
+++ JavaAdvanced24/StreamsFilesAndDirectoriesLab/G01ReadFile.java
@@ -0,0 +1,20 @@
+package StreamsFilesAndDirectoriesLab;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class G01ReadFile {
+    public static void main(String[] args) throws IOException {
+
+        String path = "/home/baldar/Documents/GitHub/JavaAdvanced24/JavaAdvanced24/StreamsFilesAndDirectoriesLab/input.txt";
+
+        FileInputStream inputStream = new FileInputStream(path);
+
+        int asciiCode = inputStream.read();
+
+        while (asciiCode != -1) {
+            System.out.print(Integer.toBinaryString(asciiCode) + " ");
+            asciiCode = inputStream.read();
+        }
+    }
+}
